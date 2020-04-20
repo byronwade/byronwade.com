3b4446ef64913d3ea85e0d59b360a4462932db23
Byron Wade <bcw1995@gmail.com>
2020-04-19 19:26:05 -0700

    changed a few things in the code block

--- byronwade.com/src/components/blocks/core/Code.tsx
+++ byronwade.com/src/components/blocks/core/Code.tsx
@@ -1,6 +1,20 @@
 import React, { useEffect } from 'react'
 import Prism from "prismjs";
 
+require('prismjs/plugins/autolinker/prism-autolinker');
+//require('prismjs/plugins/autoloader/prism-autoloader');
+require('prismjs/plugins/line-numbers/prism-line-numbers');
+
+require("prismjs/components/prism-git")
+require("prismjs/components/prism-typescript")
+require("prismjs/components/prism-bash")
+require("prismjs/components/prism-python")
+require("prismjs/components/prism-docker")
+require("prismjs/components/prism-nginx")
+//require("prismjs/components/prism-php")
+require("prismjs/components/prism-scss")
+require("prismjs/components/prism-json")
+
 type CodeComponentProps = {
   attributes: {
     className: string
@@ -25,7 +39,7 @@ const CodeComponent = ({
   
   if(codeContent) {
     return (
-      <pre className={className}>
+      <pre className={`line-numbers `+className}>
         <code>
           {codeContent}
         </code>
