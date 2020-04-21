c51f63baa4e0a36ba91a030bc78ce1e568a6ac3d
Byron Wade <bcw1995@gmail.com>
2020-04-20 20:00:58 -0700

    moved the catalog, i commented out some logging and fixed some graphql errors

--- byronwade.com/src/components/blocks/BlockList.js
+++ byronwade.com/src/components/blocks/BlockList.js
@@ -4,12 +4,12 @@ import { randomID } from "../utils/helpers"
 import ReactHtmlParser from 'react-html-parser'; //parse html
 
 export default ({blocks, content}) => {
-    console.log(blocks.length)
+    //console.log(blocks.length)
     return <>
     {blocks.length > 3 ? (
         blocks.map(block => (<Block key={`component-${randomID()}`} block={block} />))
     ) : (
-        console.log("This is showing because this page is showing content from wordpress core query and not using the blocks query"),
+        //console.log("This is showing because this page is showing content from wordpress core query and not using the blocks query"),
         ReactHtmlParser(content)
     )}
     </>
