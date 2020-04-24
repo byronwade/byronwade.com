8bc4e3fc341a31e343661b9c6776a03d69ef2200
Byron Wade <bcw1995@gmail.com>
2020-04-24 12:01:08 -0700

    added more to lazy-loading

--- byronwade.com/src/components/blocks/core/Youtube.tsx
+++ byronwade.com/src/components/blocks/core/Youtube.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import LazyLoad from 'react-lazyload';
 import { YouTubeGetID } from '../../utils/helpers'
 
 type YouTubeComponentProps = {
@@ -17,7 +18,9 @@ const YouTubeComponent = ({
 
   if(url) {
     return (
-      <iframe width="560" height="315" className={className} src={`//www.youtube.com/embed/`+YouTubeGetID(url)} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <LazyLoad placeholder={<img src="https://via.placeholder.com/560x315" alt="Image Place Holder" />}>
+        <iframe width="560" height="315" className={className} src={`//www.youtube.com/embed/`+YouTubeGetID(url)} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </LazyLoad>
     )
   }
   return null;
