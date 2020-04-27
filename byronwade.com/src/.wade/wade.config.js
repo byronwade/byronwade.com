dff36625619b713c85c968bc7b59a4627b84b8e5
Byron Wade <bcw1995@gmail.com>
2020-04-27 13:45:46 -0700

    config change

--- byronwade.com/src/.wade/wade.config.js
+++ byronwade.com/src/.wade/wade.config.js
@@ -5,7 +5,11 @@ module.exports = {
         title: `Byron Wades Web Development 2`,
         description: `Website Design and Development Firm 2`,
         author: `@byronwade18-2`,
-        siteURL: `http://64.225.119.202`
+        siteURL: `http://64.225.119.202`,
+        permalinks: {
+            base: `/__admin`,
+            posts: `Slug`  //Slug (base/sample-post/), MonthAndSlug (base/2020/04/sample-post/)
+        }
     },
     theme: {
         enable: true,
@@ -36,53 +40,7 @@ module.exports = {
             theme: 'wade-dark',
             entry: path.resolve(`./src/.wade/wades-admin/index.js`),
             permalink: {
-                base: `/__admin`,
-            },
-        }
-    },
-    has: {
-        //Blog enabled from wordpress site on a specific url
-        blog: {
-            enable: true,
-            seo: true,
-            permalink: {
-                base: `/blog`,
-                posts: `Slug` //Slug (base/sample-post/), MonthAndSlug (base/2020/04/sample-post/)
-            },
-            morePosts: {
-                facebook: {
-                    enabled: true //this enables facebook page posts inside your blog
-                },
-                rss: {
-                    enabled: true //this enabled rss feeds inside your blogs
-                }
-            }
-        },
-        //forums for websites - Wordpress Plugin used would be BBPress
-        forums: {
-            enable: true,
-            seo: true,
-            permalink: {
-                base: `/forums`,
-                posts: `Slug` //Slug (base/sample-post/), MonthAndSlug (base//2020/04/sample-post/)
-            },
-        },
-        //case studies for websites
-        cases: {
-            enable: true,
-            seo: true,
-            permalink: {
-                base: `/case-studies`,
-                posts: `Slug` //Slug (base/sample-post/), MonthAndSlug (base/2020/04/sample-post/)
-            },
-        },
-        //works would be like a profile
-        works: {
-            enable: true,
-            seo: true,
-            permalink: {
-                base: `/works`,
-                posts: `Slug` //Slug (base/sample-post/), MonthAndSlug (base/2020/04/sample-post/)
+                base: `/__admin`
             },
         }
     }
