1a2738f14343c55af47be09da7bd2d5ca5caebaf
Byron Wade <bcw1995@gmail.com>
2020-06-01 22:39:35 -0700

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,19 +51,19 @@ module.exports = {
 				// refetchInterval: 600,
 			},
 		},
-		{
-			resolve: "gatsby-source-graphql",
-			options: {
-				typeName: "GitHub",
-				fieldName: "github",
-				url: "https://api.github.com/graphql",
-				// HTTP headers
-				headers: {
-				  // Learn about environment variables: https://gatsby.dev/env-vars
-				  Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-				},
-			  },
-		},
+		// {S
+		// 	resolve: "gatsby-source-graphql",
+		// 	options: {
+		// 		typeName: "GitHub",
+		// 		fieldName: "github",
+		// 		url: "https://api.github.com/graphql",
+		// 		// HTTP headers
+		// 		headers: {
+		// 		  // Learn about environment variables: https://gatsby.dev/env-vars
+		// 		  Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+		// 		},
+		// 	  },
+		// },
 		`gatsby-plugin-react-helmet`,
 		{
 			resolve: `gatsby-source-filesystem`,
