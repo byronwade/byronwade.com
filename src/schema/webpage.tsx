a23dc03af1d6451c854a35a06650d5b011357248
Byron Wade <bcw1995@gmail.com>
2020-06-02 16:56:03 -0700

    schema test

--- src/schema/webpage.tsx
+++ src/schema/webpage.tsx
@@ -1,10 +1,10 @@
 export const SchemaWebPage = props => {
   console.log(props)
   const WebPage = {
-    "@context": "https://schema.org",
-    "@type": "WebPage",
-    "name": "Byron Wades Website Developemnt",
-    "description": "Web design and development"
+    "@context": "https://schema.org/",
+    "@type": "WebSite",
+    "name": "Byron Wades Web Development",
+    "url": "https://byronwade.com"
   };
   return JSON.stringify(WebPage);
 }
