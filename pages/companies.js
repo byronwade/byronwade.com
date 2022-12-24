a83d446fd693c48dad56d671f1112998e75b1cfa
Byron Wade <bcw1995@gmail.com>
2022-12-24 01:17:07 -0800

    more

--- pages/companies.js
+++ pages/companies.js
@@ -1,13 +1,15 @@
 //import React, { useState } from 'react';
-import PageContent from "./components/pageContent";
+import PageContent from "../components/pageContent";
 
 export default function Companys() {
 	return (
 		<PageContent>
-		<div className="controlls">
-			<h1>Home</h1>
-		</div>
-		<div className="pageContent">index</div>
+			<div className='controlls'>
+				<h1>Companys</h1>
+			</div>
+			<div className='pageContent'>
+				Wade's Plumbing & Septic/Clog Monsters Septic Pumping - 1.2m
+			</div>
 		</PageContent>
 	);
 }
