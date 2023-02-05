4ff921cda4bf91a00d10a0a8d5f510c92bcb3149
Byron Wade <bcw1995@gmail.com>
2023-02-05 04:49:39 -0800

    fixed deployment issues

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,46 +1,27 @@
 "use client";
 
 import clsx from "clsx";
-import { motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const navItems = {
 	"/": {
 		name: "home",
-		x: 0,
-		y: 0,
-		w: "64px",
 	},
 	"/about": {
 		name: "about",
-		x: 0,
-		y: 35,
-		w: "65px",
 	},
 	"/playground": {
 		name: "playground",
-		x: 0,
-		y: 69,
-		w: "110px",
 	},
 	"/portfolio": {
 		name: "portfolio",
-		x: 0,
-		y: 104,
-		w: "90px",
 	},
 	"/blog": {
 		name: "blog",
-		x: 0,
-		y: 138,
-		w: "56px",
 	},
 	"/guestbook": {
 		name: "guestbook",
-		x: 0,
-		y: 174,
-		w: "100px",
 	},
 };
 
@@ -79,47 +60,6 @@ export default function Navbar() {
 					className='flex flex-row md:flex-col items-start relative overflow-scroll px-4 md:px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative'
 					id='nav'>
 					<div className='flex flex-row md:flex-col space-x-0 pr-10 mb-2 mt-2 md:mt-0'>
-						{navItems[pathname] ? (
-							<>
-								{/* Desktop version, hidden on mobile, animates y axis */}
-								<div className='hidden md:block'>
-									<motion.div
-										className='absolute bg-neutral-100 dark:bg-neutral-800 h-[34px] rounded-md z-[-1]'
-										layoutId='test2'
-										initial={{ opacity: 0, y: navItems[pathname].y }}
-										animate={{
-											opacity: 1,
-											y: navItems[pathname].y,
-											width: navItems[pathname].w,
-										}}
-										transition={{
-											type: "spring",
-											stiffness: 350,
-											damping: 30,
-										}}
-									/>
-								</div>
-								{/* Mobile version, hidden on desktop, animates x axis */}
-								<div className='block md:hidden'>
-									<motion.div
-										className='absolute bg-neutral-100 dark:bg-neutral-800 h-[34px] rounded-md z-[-1]'
-										layoutId='test'
-										initial={{ opacity: 0, x: navItems[pathname].x }}
-										animate={{
-											opacity: 1,
-											x: navItems[pathname].x,
-											width: navItems[pathname].w,
-										}}
-										transition={{
-											type: "spring",
-											stiffness: 350,
-											damping: 30,
-										}}
-									/>
-								</div>
-							</>
-						) : null}
-
 						{Object.entries(navItems).map(([path, { name }]) => {
 							const isActive = path === pathname;
 
