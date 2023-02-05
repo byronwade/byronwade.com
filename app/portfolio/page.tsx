363952064ae25d56180594cd8caeeb48d8a3196e
Byron Wade <bcw1995@gmail.com>
2023-02-05 04:54:44 -0800

    fixed deployment issues

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -17,38 +17,30 @@ export default async function Portfolio() {
 		<section>
 			<h1 className='font-bold text-3xl'>Portfolio</h1>
 			<div className='prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200'>
-				{getDribbbles[1] !== 1 ? (
-					getDribbbles.map((repo, index) => {
-						return (
-							<div key={index}>
-								<img src={repo.images.hidpi} />
-								<Link href={repo.html_url}>{repo.title}</Link>
-								<p>{repo.description.replace(/<[^>]*>/g, "")}</p>
-							</div>
-						);
-					})
-				) : (
-					<p>API Rate limit Exceeded</p>
-				)}
-				{getRepo[1] !== 1 ? (
-					getRepo.getRepo.map((repo, index) => {
-						return (
-							<div key={index}>
-								<h2>{repo.name}</h2>
-								<Link href={repo.url}>{repo.full_name}</Link>
-								<p>
-									Stars: {repo.stars} - Watchers: {repo.watchers} - Forks{" "}
-									{repo.forks}
-								</p>
-								<p>{repo.language}</p>
-								<p>{new Date(repo.created_at).toLocaleString()}</p>
-								<p>{new Date(repo.updated_at).toLocaleString()}</p>
-							</div>
-						);
-					})
-				) : (
-					<p>API Rate limit Exceeded</p>
-				)}
+				{getDribbbles.map((repo, index) => {
+					return (
+						<div key={index}>
+							<img src={repo.images.normal} />
+							<Link href={repo.html_url}>{repo.title}</Link>
+							<p>{repo.description.replace(/<[^>]*>/g, "")}</p>
+						</div>
+					);
+				})}
+				{getRepo.getRepo.map((repo, index) => {
+					return (
+						<div key={index}>
+							<h2>{repo.name}</h2>
+							<Link href={repo.url}>{repo.full_name}</Link>
+							<p>
+								Stars: {repo.stars} - Watchers: {repo.watchers} - Forks{" "}
+								{repo.forks}
+							</p>
+							<p>{repo.language}</p>
+							<p>{new Date(repo.created_at).toLocaleString()}</p>
+							<p>{new Date(repo.updated_at).toLocaleString()}</p>
+						</div>
+					);
+				})}
 			</div>
 		</section>
 	);
