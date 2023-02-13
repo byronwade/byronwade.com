cc2230bfda73ad24def83066ceacc419830da499
Byron Wade <bcw1995@gmail.com>
2023-02-13 01:14:45 -0800

    blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,15 +2,10 @@ import { allBlogs } from "contentlayer/generated";
 import Link from "next/link";
 import ViewCounter from "./view-counter";
 
-export const metadata = {
-	title: "Blog",
-	description: "Read my thoughts on software development, design, and more.",
-};
-
 export default async function BlogPage() {
 	return (
 		<section>
-			<h1 className='font-bold text-3xl mb-5'>Blog</h1>
+			<h1 className="font-bold text-3xl mb-5">Blog</h1>
 			{allBlogs
 				.sort((a, b) => {
 					if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
@@ -21,9 +16,10 @@ export default async function BlogPage() {
 				.map((post) => (
 					<Link
 						key={post.slug}
-						className='flex flex-col space-y-1 mb-4'
-						href={`/blog/${post.slug}`}>
-						<div className='w-full flex flex-col'>
+						className="flex flex-col space-y-1 mb-4"
+						href={`/blog/${post.slug}`}
+					>
+						<div className="w-full flex flex-col">
 							<p>{post.title}</p>
 							<ViewCounter slug={post.slug} trackView={false} />
 						</div>
