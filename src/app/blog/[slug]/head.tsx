8ba25c7f793d44987f8bcbabdd093a9bf237bc80
Byron Wade <bcw1995@gmail.com>
2023-02-15 00:52:57 -0800

    wordpress blog

--- src/app/blog/[slug]/head.tsx
+++ src/app/blog/[slug]/head.tsx
@@ -1,37 +1,51 @@
-import { allBlogs } from "contentlayer/generated";
+"use client";
+import { useEffect, useState } from "react";
+import graphQLClient from "src/components/graphql-client";
+import { GET_POST_BY_SLUG } from "./GET_POST_BY_SLUG";
 
-export default async function Head({ params }) {
-	const post = allBlogs.find((post) => post.slug === params.slug) || {
-		title: "Not Found",
-		summary: "This page could not be found.",
-		image: "https://byronwade.com/api/og?title=Not%20Found",
-		publishedAt: new Date().toISOString(),
-	};
+export default function Head({ params }) {
+	const [post, setPost] = useState({
+		title: "",
+		excerpt: "",
+		featuredImage: "",
+		date: "",
+	});
+	useEffect(() => {
+		graphQLClient
+			.request(GET_POST_BY_SLUG, { slug: params.slug })
+			.then((data) => {
+				setPost(data.postBy);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, [params.slug]);
 
-	const ogImage = post.image
-		? post.image
+	const excerpt = post.excerpt.replace(/<[^>]+>/g, "");
+	const ogImage = post.featuredImage
+		? post.featuredImage
 		: `https://byronwade.com/api/og?title=${post.title}`;
 
 	return (
 		<>
 			<title>{`${post.title} – Byron Wade`}</title>
-			<meta content={post.summary} name='description' />
+			<meta content={excerpt} name="description" />
 			<meta
-				property='og:url'
+				property="og:url"
 				content={`https://byronwade.com/blog/${params.slug}`}
 			/>
 			<link
-				rel='canonical'
+				rel="canonical"
 				href={`https://byronwade.com/blog/${params.slug}`}
 			/>
-			<meta property='og:description' content={post.summary} />
-			<meta property='og:title' content={post.title} />
-			<meta property='og:image' content={ogImage} />
-			<meta property='og:image:url' content={ogImage}></meta>
-			<meta name='twitter:title' content={post.title} />
-			<meta name='twitter:description' content={post.summary} />
-			<meta name='twitter:image' content={ogImage} />
-			<meta property='article:published_time' content={post.publishedAt} />
+			<meta property="og:description" content={excerpt} />
+			<meta property="og:title" content={post.title} />
+			<meta property="og:image" content={ogImage} />
+			<meta property="og:image:url" content={ogImage}></meta>
+			<meta name="twitter:title" content={post.title} />
+			<meta name="twitter:description" content={excerpt} />
+			<meta name="twitter:image" content={ogImage} />
+			<meta property="article:published_time" content={post.date} />
 		</>
 	);
 }
