bc490a3fa4f809a71a9c81a604de2c4228b5788c
Byron Wade <bcw1995@gmail.com>
2023-02-14 23:58:43 -0800

    wordpress blog

---
+++ src/app/test/[slug]/head.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useEffect, useState } from "react";
+import graphQLClient from "src/components/graphql-client";
+import { GET_POST_BY_SLUG } from "./GET_POST_BY_SLUG";
+
+export default function Head({ params }) {
+	const [post, setPost] = useState({
+		title: "",
+		excerpt: "",
+		featuredImage: "",
+		date: "",
+	});
+	useEffect(() => {
+		graphQLClient
+			.request(GET_POST_BY_SLUG, { slug: params.slug })
+			.then((data) => {
+				setPost(data.postBy);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, [params.slug]);
+
+	const excerpt = post.excerpt.replace(/<[^>]+>/g, "");
+	const ogImage = post.featuredImage
+		? post.featuredImage
+		: `https://byronwade.com/api/og?title=${post.title}`;
+
+	return (
+		<>
+			<title>{`${post.title} – Byron Wade`}</title>
+			<meta content={excerpt} name="description" />
+			<meta
+				property="og:url"
+				content={`https://byronwade.com/blog/${params.slug}`}
+			/>
+			<link
+				rel="canonical"
+				href={`https://byronwade.com/blog/${params.slug}`}
+			/>
+			<meta property="og:description" content={excerpt} />
+			<meta property="og:title" content={post.title} />
+			<meta property="og:image" content={ogImage} />
+			<meta property="og:image:url" content={ogImage}></meta>
+			<meta name="twitter:title" content={post.title} />
+			<meta name="twitter:description" content={excerpt} />
+			<meta name="twitter:image" content={ogImage} />
+			<meta property="article:published_time" content={post.date} />
+		</>
+	);
+}
---
+++ src/app/test/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import graphQLClient from "../../components/graphql-client";
+import { useEffect, useState } from "react";
+import { GET_POSTS } from "./GET_POSTS";
+import ViewCounter from "./view-counter";
+import Link from "next/link";
+
+export default function Blogs() {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		graphQLClient
+			.request(GET_POSTS)
+			.then((data) => {
+				setPosts(data.posts.nodes);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, []);
+
+	if (!posts) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<section className="pb-40">
+			<h1 className="font-bold text-3xl mb-5">Blog</h1>
+			{posts.map((post) => (
+				<Link
+					key={post.slug}
+					className="flex flex-col space-y-1 mb-4"
+					href={`/test/${post.slug}`}
+				>
+					<div className="w-full flex flex-col">
+						<p>{post.title}</p>
+						<ViewCounter slug={post.slug} trackView={false} />
+					</div>
+				</Link>
+			))}
+		</section>
+	);
+}
