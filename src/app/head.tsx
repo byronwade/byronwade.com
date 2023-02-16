127b62c7ee0aaed6c9bf6a70e7ae7b443752a72a
Byron Wade <bcw1995@gmail.com>
2023-02-15 18:07:25 -0800

    added next-seo to all pages and some JSON-LD content will be added later

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,22 @@
-import { NextSeo } from 'next-seo';
-import { NEXT_SEO_DEFAULT } from '../../next-seo.config';
+import { NextSeo, SocialProfileJsonLd } from "next-seo";
+import { NEXT_SEO_DEFAULT } from "../../next-seo.config";
 
 export default function Head() {
-  return (
-    <>
-      <NextSeo {...NEXT_SEO_DEFAULT} useAppDir={true} />
-    </>
-  )
+	return (
+		<>
+			<NextSeo {...NEXT_SEO_DEFAULT} useAppDir={true} />
+			<SocialProfileJsonLd
+				useAppDir={true}
+				type="Person"
+				name="your name"
+				url="http://www.your-site.com"
+				sameAs={[
+					"http://www.facebook.com/your-profile",
+					"http://instagram.com/yourProfile",
+					"http://www.linkedin.com/in/yourprofile",
+					"http://plus.google.com/your_profile",
+				]}
+			/>
+		</>
+	);
 }
