cb8401e312fd6c418cee3c12cabdb2f15844565c
Byron Wade <bcw1995@gmail.com>
2023-02-15 18:24:28 -0800

    server sitemap generator updated

--- src/pages/server-sitemap-index.xml/index.tsx
+++ src/pages/server-sitemap-index.xml/index.tsx
@@ -1,15 +1,31 @@
 // pages/server-sitemap-index.xml/index.tsx
 import { getServerSideSitemapIndex } from "next-sitemap";
 import { GetServerSideProps } from "next";
+import { gql } from "graphql-request";
+import graphQLClient from "../../lib/graphql-client";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	// Method to source urls from cms
-	// const urls = await fetch('https//example.com/api')
+	// Query all blog posts
+	const query = gql`
+		query AllBlogPosts {
+			posts {
+				edges {
+					node {
+						slug
+					}
+				}
+			}
+		}
+	`;
 
-	return getServerSideSitemapIndex(ctx, [
-		"https://example.com/path-1.xml",
-		"https://example.com/path-2.xml",
-	]);
+	const { posts } = await graphQLClient.request(query);
+
+	// Map blog post slugs to URLs
+	const urls = posts.edges.map(
+		({ node }) => `https://byronwade.com/${node.slug}`
+	);
+
+	return getServerSideSitemapIndex(ctx, urls);
 };
 
 // Default export to prevent next.js errors
