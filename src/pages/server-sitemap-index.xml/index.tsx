63ba049341d7367ac9ecb4660f57d693d5cf3070
Byron Wade <bcw1995@gmail.com>
2023-02-15 18:38:36 -0800

    server sitemap generator did not work

--- src/pages/server-sitemap-index.xml/index.tsx
+++ src/pages/server-sitemap-index.xml/index.tsx
@@ -1,32 +1,32 @@
-// pages/server-sitemap-index.xml/index.tsx
-import { getServerSideSitemapIndex } from "next-sitemap";
-import { GetServerSideProps } from "next";
-import { gql } from "graphql-request";
-import graphQLClient from "../../lib/graphql-client";
+// // pages/server-sitemap-index.xml/index.tsx
+// import { getServerSideSitemapIndex } from "next-sitemap";
+// import { GetServerSideProps } from "next";
+// import { gql } from "graphql-request";
+// import graphQLClient from "../../lib/graphql-client";
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	// Query all blog posts
-	const query = gql`
-		query AllBlogPosts {
-			posts {
-				edges {
-					node {
-						slug
-					}
-				}
-			}
-		}
-	`;
+// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+// 	// Query all blog posts
+// 	const query = gql`
+// 		query AllBlogPosts {
+// 			posts {
+// 				edges {
+// 					node {
+// 						slug
+// 					}
+// 				}
+// 			}
+// 		}
+// 	`;
 
-	const { posts } = await graphQLClient.request(query);
+// 	const { posts } = await graphQLClient.request(query);
 
-	// Map blog post slugs to URLs
-	const urls = posts.edges.map(
-		({ node }) => `https://byronwade.com/blog/${node.slug}`
-	);
+// 	// Map blog post slugs to URLs
+// 	const urls = posts.edges.map(
+// 		({ node }) => `https://byronwade.com/blog/${node.slug}`
+// 	);
 
-	return getServerSideSitemapIndex(ctx, urls);
-};
+// 	return getServerSideSitemapIndex(ctx, urls);
+// };
 
-// Default export to prevent next.js errors
-export default function SitemapIndex() {}
+// // Default export to prevent next.js errors
+// export default function SitemapIndex() {}
