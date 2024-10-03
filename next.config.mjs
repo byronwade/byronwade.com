5c6d66804a090f92cf2d10c3e6bd141918b191c0
Byron Wade <bcw1995@gmail.com>
2024-10-03 12:02:04 -0400

    analytics

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,7 @@
 const nextConfig = {
 	experimental: {
 		nextScriptWorkers: true,
-	},
-    async headers() {
-        return [
-          {
-            source: '/(.*)',
-            headers: [
-              {
-                key: 'Content-Security-Policy',
-                value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:;"
-              },
-            ],
-          },
-        ];
-      },
+	}
 };
 
 export default nextConfig;
