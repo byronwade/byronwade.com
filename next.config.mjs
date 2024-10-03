809dabd6e97f91b14d2ebe8afc4611dec889989a
Byron Wade <bcw1995@gmail.com>
2024-10-03 16:13:39 -0400

    more

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['localhost'], // Add any other domains you're using for images
+  },
+};
 
 export default nextConfig;
