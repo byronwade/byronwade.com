2c065ecc195f56572cfccca19f10b562c29b1975
Byron Wade <bcw1995@gmail.com>
2024-10-03 07:03:34 -0400

    more

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:;"
+              },
+            ],
+          },
+        ];
+      },
+};
 
 export default nextConfig;
