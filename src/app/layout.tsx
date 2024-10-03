76dd4e3953c2fa137adddd797006dbc0d4faca9f
Byron Wade <bcw1995@gmail.com>
2024-10-03 11:02:06 -0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
@@ -21,6 +22,16 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={GeistSans.className}>
+			<Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-WE1RMQ935W"></Script>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WE1RMQ935W');
+					`}
+			</Script>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
 					<Header />
 					{children}
